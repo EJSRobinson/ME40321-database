7aa041eb8eca2976087022db8b657cf7f4969489
EJSRobinson <elliotjs.robinson@gmail.com>
2022-03-02 13:41:55 +0000

    Add core context properties

---
+++ context-core.js
@@ -0,0 +1,17 @@
+export const cont = {
+  1000: {
+    id: 1000,
+    name: 'Centre of Gravity Position',
+    shortName: 'XCog',
+  },
+  1001: {
+    id: 1001,
+    name: 'Maximum Altitude',
+    shortName: 'AltMax',
+  },
+  1002: {
+    id: 1002,
+    name: 'Maximum Launch Air Temperature',
+    shortName: 'Ta',
+  },
+};
