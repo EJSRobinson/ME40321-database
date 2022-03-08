6a9561a35da2907dd4c6273bc9c6455342b13f4e
EJSRobinson <elliotjs.robinson@gmail.com>
2022-03-08 17:46:26 +0000

    Add Types

--- base.js
+++ base.js
@@ -1,15 +1,31 @@
 import { props } from './properties-core.js';
 import { cont } from './context-core.js';
 
-const valueTemplate = { max: null, min: null };
+//TYPES
+// quant (Simple / Max Min)
+// range (Varries Over a range)
+// list (Discrete values)
+// qual (Simple string)
+
+const valueTypes = {
+  quant: { typeName: 'quant', max: null, min: null },
+  range: { typeName: 'range', max: [], min: [] },
+  list: { typeName: 'list', options: [], val: null },
+  qual: { typeName: 'qual', val: null },
+};
+
+function getTypeTemplate(type) {
+  return valueTypes[type];
+}
 
 export function getProperties() {
   let properties = new Map();
   for (const [key, value] of Object.entries(props)) {
-    properties.set(key, {
+    properties.set(value.shortName, {
+      id: key,
       name: value.name,
       shortName: value.shortName,
-      value: valueTemplate,
+      value: getTypeTemplate(value.type),
     });
   }
   return properties;
@@ -18,10 +34,11 @@ export function getProperties() {
 export function getContext() {
   let context = new Map();
   for (const [key, value] of Object.entries(cont)) {
-    context.set(key, {
+    context.set(value.shortName, {
+      id: key,
       name: value.name,
       shortName: value.shortName,
-      value: valueTemplate,
+      value: getTypeTemplate(value.type),
     });
   }
   return context;
--- properties-core.js
+++ properties-core.js
@@ -1,167 +1,218 @@
+//TYPES
+// quant (Simple / Max Min)
+// range (Varries Over a range)
+// list (Discrete values)
+// qual (Simple string)
+
 export const props = {
   0: {
     id: 0,
     name: 'Tip Chord',
     shortName: 'ct',
+    type: 'quant',
   },
   1: {
     id: 1,
     name: 'Root Chord',
     shortName: 'cr',
+    type: 'quant',
   },
   2: {
     id: 2,
     name: 'Span',
     shortName: 'S',
+    type: 'quant',
   },
   3: {
     id: 3,
     name: 'Aspect Ratio',
     shortName: 'AR',
+    type: 'quant',
   },
   4: {
     id: 4,
     name: 'Taper Ratio',
     shortName: 'TR',
+    type: 'quant',
   },
   5: {
     id: 5,
     name: 'Fin Area',
     shortName: 'Afin',
+    type: 'quant',
   },
   6: {
     id: 6,
     name: 'Lift Slope',
     shortName: 'Cla',
+    type: 'quant',
   },
   7: {
     id: 7,
     name: 'Drag Slope',
     shortName: 'Cda',
+    type: 'quant',
   },
   8: {
     id: 8,
     name: 'Angle of Attack',
     shortName: 'AoA',
+    type: 'quant',
   },
   9: {
     id: 9,
     name: 'Normal Force Coefficient',
     shortName: 'Cn',
+    type: 'quant',
   },
   10: {
     id: 10,
     name: 'Tangential Force Coefficient',
     shortName: 'Ct',
+    type: 'quant',
   },
   11: {
     id: 11,
     name: 'Lift Force Coefficient',
     shortName: 'Cl',
+    type: 'quant',
   },
   12: {
     id: 12,
     name: 'Drag Force Coefficient',
     shortName: 'Cd',
+    type: 'quant',
   },
   13: {
     id: 13,
     name: 'Lift Force',
     shortName: 'Fl',
+    type: 'quant',
   },
   14: {
     id: 14,
     name: 'Drag Force',
     shortName: 'Fl',
+    type: 'quant',
   },
   15: {
     id: 15,
     name: 'Stress',
     shortName: 'Sigma',
+    type: 'range',
   },
   16: {
     id: 16,
     name: 'Thickness',
     shortName: 't',
+    type: 'quant',
   },
   17: {
     id: 17,
     name: 'Deflection',
     shortName: 'v',
+    type: 'quant',
   },
   18: {
     id: 18,
     name: 'Mass',
     shortName: 'm',
+    type: 'quant',
   },
   19: {
     id: 19,
     name: 'Set Mass',
     shortName: 'mT',
+    type: 'quant',
   },
   20: {
     id: 20,
     name: 'Quantity of Fins in set',
     shortName: 'N',
+    type: 'quant',
   },
   21: {
     id: 21,
     name: 'Leading Edge Sweep',
     shortName: 'LEsw',
+    type: 'quant',
   },
   22: {
     id: 22,
     name: 'Trailing Edge Sweep',
     shortName: 'TEsw',
+    type: 'quant',
   },
   23: {
     id: 23,
     name: 'Axial Position',
     shortName: 'Xfin',
+    type: 'quant',
   },
   24: {
     id: 24,
     name: 'Static Stability Margin',
     shortName: 'Kn',
+    type: 'quant',
   },
   25: {
     id: 25,
     name: 'Mach Number',
     shortName: 'M',
+    type: 'quant',
   },
   26: {
     id: 26,
     name: 'Velocity',
     shortName: 'V',
+    type: 'quant',
   },
   27: {
     id: 27,
     name: 'Critical Flutter Velocity',
     shortName: 'Vcr',
+    type: 'quant',
   },
   28: {
     id: 28,
     name: 'Divergence Velocity',
     shortName: 'Vdiv',
+    type: 'quant',
   },
   29: {
     id: 29,
     name: 'Corrective Moment Coefficient',
     shortName: 'C1',
+    type: 'quant',
   },
   30: {
     id: 30,
     name: 'Natural Pitching Frequency',
     shortName: 'Wn',
+    type: 'quant',
   },
   31: {
     id: 31,
     name: 'Damping Ratio in Pitch',
     shortName: 'Zeta',
+    type: 'quant',
   },
   32: {
     id: 32,
     name: 'Pitching Time Period',
     shortName: 'Tp',
+    type: 'quant',
+  },
+  33: {
+    id: 33,
+    name: 'Material',
+    shortName: 'Mat',
+    type: 'list',
+  },
+  34: {
+    id: 34,
+    name: 'Aerfoil',
+    shortName: 'Arf',
+    type: 'qual',
   },
 };
